bd687938b628572890e3f27b31fdf67d19642a0b
Deurivaldo Barbosa <=>
2021-05-31 14:40:20 -0300

    M - adicionando mais containers

--- stopcontainers.sh
+++ stopcontainers.sh
@@ -1,3 +1,3 @@
 #! /bin/zsh
 
-docker stop jenkins-cicd gitlab-cicd registry-cicd sonarqube-cicd sonarqubedb-cicd haproxy-cicd appspring
+docker stop jenkins-cicd gitlab-cicd registry-cicd sonarqube-cicd sonarqubedb-cicd haproxy-cicd ns1-cicd ns2-cicd appspring
